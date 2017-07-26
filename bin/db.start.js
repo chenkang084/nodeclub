ccb657bf61aa4f42e12c75c09d6f3e2b198b79d7
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-07-26 21:58:32 +0800

    add comments

--- bin/db.start.js
+++ bin/db.start.js
@@ -1,12 +1,12 @@
 const shell = require("shelljs");
-
+//start mongo db
 shell.exec(
   "cd D:/Program Files/MongoDB/Server/3.4/bin && mongod.exe --dbpath ../db/node_club_dev/",
   (code, stdout, stderr) => {
       !stdout && console.log("ok")
   }
 );
-
+//redis
 shell.exec(
   "cd D:/Program Files/Redis-x64-3.2.100 && redis-server.exe redis.windows.conf",
   (code, stdout, stderr) => {
