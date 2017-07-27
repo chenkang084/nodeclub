bdc09789685ce0c85567350e93ec7de94ae7f6a5
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-07-27 18:20:08 +0800

    add promise

--- test.js
+++ test.js
@@ -1,15 +1,40 @@
-var shell = require("shelljs");
+// var shell = require("shelljs");
 
-shell.exec("cd D:/test && tail  a.log", { async: true }, function(
-  code,
-  stdout,
-  stderr
-) {
+// shell.exec("cd D:/test && tail  a.log", { async: true }, function(
+//   code,
+//   stdout,
+//   stderr
+// ) {
 
-  // iconv.decode(stderr,'gbk')
-  console.log("Exit code:", code);
-  console.log("Program output:", stdout);
-  console.log("Program stderr:", stderr);
+//   // iconv.decode(stderr,'gbk')
+//   console.log("Exit code:", code);
+//   console.log("Program output:", stdout);
+//   console.log("Program stderr:", stderr);
+// });
+
+const fs = require("fs");
+
+const convertPromise = fn => {
+  return new Promise((resolve, reject) => {
+    fn.resolve = resolve;
+    fn.reject = reject;
+    fn.call(fn);
+  });
+};
+
+const readFileFn = filename => {
+  return function() {
+    fs.readFile(filename, (err, data) => {
+      if (err) this.reject(err);
+      this.resolve(data);
+    });
+  };
+};
+
+
+Promise.all([
+  convertPromise(readFileFn("./README.md")),
+  convertPromise(readFileFn("./README.md"))
+]).then(result => {
+  console.log()
 });
-
-
